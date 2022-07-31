ace5deda5bf615f573e35ef04ff1da24a8fac1ab
Tristan Joseph Seril <tjseril@192-168-1-108.tpgi.com.au>
2022-07-31 17:25:29 +1000

    draft 1

--- cypress.config.js
+++ cypress.config.js
@@ -2,8 +2,10 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: "https://domain.com.au",
+    modifyObstructiveCode: false,
     setupNodeEvents(on, config) {
       // implement node event listeners here
-    },
-  },
+    }
+  }
 });
--- cypress/e2e/DomainTest.cy.js
+++ cypress/e2e/DomainTest.cy.js
@@ -1,5 +1,21 @@
-describe('empty spec', () => {
-  it('passes', () => {
-    cy.visit('https://www.domain.com.au')
+let testdata
+
+describe('Domain Website Testing', function() {
+  before('Load test data', function() {
+    cy.fixture('propsearchdata').then(function(propsearchdata){
+      testdata = propsearchdata
+    })
+  })
+
+  it('Open Domain homepage and check elements', function() {
+    cy.visit('/')
+    expect(cy.get('#fe-pa-domain-home-typeahead-input')).to.exist
+    expect(cy.get('.css-oih30v')).to.exist
+  })
+
+  it('Search using string STR', function() {
+    cy.get('#fe-pa-domain-home-typeahead-input').type(testdata.suburb)
+    cy.get('.css-oih30v').click()
+    expect(cy.get('.css-1biynnu')).to.exist
   })
 })
