f173ad19eca17cdbde864c0309eb0a073a05c166
Tristan Joseph Seril <tjseril@192-168-1-108.tpgi.com.au>
2022-08-07 18:35:57 +1000

    added 3 tests

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,10 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  screenshotOnRunFailure: true,
+  video: false,
   e2e: {
     baseUrl: "https://domain.com.au",
-    modifyObstructiveCode: false,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     }
--- cypress/e2e/DomainTest.cy.js
+++ cypress/e2e/DomainTest.cy.js
@@ -1,6 +1,7 @@
-let testdata
-
 describe('Domain Website Testing', function() {
+
+  let testdata
+
   before('Load test data', function() {
     cy.fixture('propsearchdata').then(function(propsearchdata){
       testdata = propsearchdata
@@ -8,14 +9,23 @@ describe('Domain Website Testing', function() {
   })
 
   it('Open Domain homepage and check elements', function() {
-    cy.visit('/')
+    cy.visit({url: "/", headers: { "Accept-Encoding": "gzip, deflate"}})
     expect(cy.get('#fe-pa-domain-home-typeahead-input')).to.exist
     expect(cy.get('.css-oih30v')).to.exist
   })
 
   it('Search using string STR', function() {
-    cy.get('#fe-pa-domain-home-typeahead-input').type(testdata.suburb)
+    cy.get('#fe-pa-domain-home-typeahead-input').type(testdata.keyword)
     cy.get('.css-oih30v').click()
-    expect(cy.get('.css-1biynnu')).to.exist
+    cy.get('.is-first-in-list.css-1qp9106').should.exist
+  })
+
+  it('Open first result and verify listing matches keyword search', function() {
+    var addressLine1
+    var addressLine1Element = '#skip-link-content > div.css-1ned5tb > div.css-1mf5g4s > ul > li.is-first-in-list.css-1qp9106 > div > div.css-1n74r2t > div > a > h2 > span:nth-child(1)'
+    cy.get(addressLine1Element).then($addressLine1Text, function() {
+      addressLine1 = $addressLine1Text
+    })
+    cy.log(addressLine1)
   })
 })
